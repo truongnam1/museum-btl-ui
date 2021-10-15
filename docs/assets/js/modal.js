fe51e4cd52b3332b23cc7ec0a036343738e30ca3
19020440 <you@example.com>
2021-10-15 22:45:21 +0700

    thang

---
+++ docs/assets/js/modal.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.modal-topic_img-image').okzoom({
+        width: 200,
+        height: 200,
+        scaleWidth: 800,
+        round: true,
+        background: "#fff",
+        // backgroundRepeat: "repeat",
+        shadow: "0 0 5px #000",
+        border: "1px solid black"
+        });
+})
