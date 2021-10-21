4d93858ad48b2dbfbe4af75e5a21ac68ea66e378
19020440 <you@example.com>
2021-10-21 16:19:25 +0700

    thang

--- docs/assets/js/DataMuseum.js
+++ docs/assets/js/DataMuseum.js
@@ -83,9 +83,52 @@ window.DataMuseum = (function() {
                                 </div>
 
                                 <!-- Modal body -->
-                                <div class="modal-body">
-                                <div class="image"><img class="card-img" style="object-fit: none;" src="${item["url_full"]}" alt="Card image"> </div>
-                                ${item["desc"]}
+                                <div class="modal-body modal-body-t">
+                                <div class="modal-topic">
+                                <div class="row modal-topic_body">
+                                        <div class="col-xl-6 modal-topic_img">
+                                        <img src="${item["url_full"]}" class="modal-topic_img-image"/>
+                                            <!--<model-viewer alt="" src="assets/image/scene.gltf" seamless-poster shadow-intensity="1" camera-controls class="model_viewer"></model-viewer> -->
+                                        </div>
+                                        
+                                        <div class="col-xl-6 modal-topic_content" >
+                                            <div class="row">
+                                                <div class="col-xl-12 modal-topic_content-header">
+                                                    <div class="modal-topic_content-icon">
+                                                        <i class="fas fa-share-alt"></i>
+                                                        <i class="far fa-flag"></i>
+                                                    </div>
+                                                    <div class="modal-topic_content-button_save">
+                                                        <span>Hồ Chí Minh</span>
+                                                        <i class="fas fa-chevron-down"></i>
+                                                        <button class="modal-btn_save">Lưu</button>
+                                                    </div>
+                                                </div>
+                
+                                                <div class="col-xl-12 modal-topic_content-header1">
+                                                    <h1>Chủ Tịch Hồ Chí Minh</h1>
+                                                    <div class="modal-topic_content-header1-group">
+                                                        <div class="modal-img_group">
+                                                            <img src="assets/image/Bot-avt.png" alt="">
+                                                            <span>Nhóm 8</span>
+                                                        </div>
+                                                        <button class="modal-btn_follow">Theo dõi</button>
+                                                    </div>
+                                                    <i class="far fa-heart">  <span> 142</span> </i>
+                                                </div>
+                
+                                                <div class="col-xl-12 modal-topic_content-word">
+                                                    <h3 style="font-weigth: 600">Tuổi trẻ</h3>
+                                                    <p class="col-xl-12">Hồ Chí Minh (Chữ Nho: 胡志明; 19 tháng 5 năm 1890 – 2 tháng 9 năm 1969), tên khai sinh là Nguyễn Sinh Cung (Chữ Nho: 阮生恭), là một nhà cách mạng và chính khách người Việt Nam. Ông là người sáng lập ra Đảng Cộng sản Việt Nam, từng là Thủ tướng Việt Nam Dân chủ Cộng hòa trong những năm 1945–1955 và là Chủ tịch nước Việt Nam Dân chủ Cộng hòa từ năm 1951 đến khi qua đời. Ở Việt Nam, ông thường được gọi là Bác Hồ.</p>
+                                                    <p class="col-xl-12">Trong quãng thời gian sinh sống và hoạt động trước khi lên nắm quyền, Hồ Chí Minh đã đi qua nhiều quốc gia và châu lục, ông được cho là đã sử dụng 50[2] đến 200 bí danh khác nhau.[3] Về mặt tư tưởng chính trị, Hồ Chí Minh là một người theo chủ nghĩa Marx – Lenin.</p>
+                                                    <p class="col-xl-12">
+                                                        Ông là lãnh đạo phong trào độc lập Việt Minh, tiến hành Cách mạng Tháng Tám năm 1945 và thành lập nước Việt Nam Dân chủ Cộng hòa sau cuộc tổng tuyển cử năm 1946. Sau chiến thắng Điện Biên Phủ năm 1954, chiến tranh Đông Dương chấm dứt. Trong giai đoạn diễn ra chiến tranh Việt Nam, Hồ Chí Minh là nhân vật chủ chốt trong hàng ngũ lãnh đạo của Việt Nam Dân chủ Cộng hòa ở miền Bắc Việt Nam và Mặt trận Dân tộc Giải phóng miền Nam Việt Nam
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
                                 </div>
 
                                 <!-- Modal footer -->
@@ -96,6 +139,9 @@ window.DataMuseum = (function() {
                             </div>
                         </div>
         `;
+        // let html = `
+       
+        // `
         modal.innerHTML = html;
     }
 
